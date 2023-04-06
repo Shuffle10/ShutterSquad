f7ef42666cc370317846f5c3aa6f779831bf9612
shuffle10 <saphal.pant@westcliff.edu>
2023-04-06 14:52:24 +0545

    Custom fetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,9 +1,36 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = (url) => {
+const useFetch = (endpoint) => {
+  const URL = "http://localhost:8000/";
   const [data, setData] = useState(null);
-  useEffect(() => {}, []);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get(URL + endpoint)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch data!");
+          } else {
+            return res.json;
+          }
+        })
+        .then((data) => {
+          setData(data);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+        });
+    }, 2000);
+  }, [endpoint]);
+
+  return { data, loading, error };
 };
 
 export default useFetch;
